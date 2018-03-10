cd9715b33f7ebebbe0a78133e0851b05ba30301b
Madhan Kumar S <35567949+madhankumardroid@users.noreply.github.com>
2018-03-10 14:36:28 +0530

    Indentation changes
    Made alignments.

--- ClassesandObjects/src/CaseStudy.java
+++ ClassesandObjects/src/CaseStudy.java
@@ -1,61 +1,51 @@
 import java.io.*;
 class PersonalDetail {
-String name;
-int age;
-long mobileno;
-String location;
+    private String name;
+    private int age;
+    private long mobileNo;
+    private String location;
 
-public PersonalDetail(String persName) {  //Constructor with parameters
-	
-	name=persName;
+    PersonalDetail(String personName) {  //Constructor with parameters
+        name = personName;
+    }
 
+    void setAge(int persAge) {
+        age = persAge; //assign persAge to age
+    }
+
+    void setMobile(long persMobile) {
+        mobileNo = persMobile; //assign persMobile to mobileNo
+    }
+
+    public void setLocation(String persLoc) {
+        location = persLoc; //assign persLoc to location
+    }
+
+    void printDetail() {
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("Mobile Number : " + mobileNo);
+        System.out.println("Location : " + location);
+    }
 }
 
-public void setAge(int persAge) {
-	
-	age=persAge; //assign persAge to age
-	
-}
-
-public void setMobile(long persMobile) {
-	
-	mobileno=persMobile; //assign persMobile to mobileno
-	
-}
-
-public void setLocation(String persLoc) {
-	
-	location=persLoc; //assign persLoc to location
-	
-}
-
-public void printDetail() {
-	System.out.println("Name : "+name);
-	System.out.println("Age : "+age);
-	System.out.println("Mobile Number : "+mobileno);
-	System.out.println("Location : "+location);
-}
-}
 public class CaseStudy {
+    public static void main(String[] args) {
+        //creating objects using constructors
+        PersonalDetail perOne = new PersonalDetail("Madhan Kumar S");
+        PersonalDetail perTwo = new PersonalDetail("Joe");
 
-	public static void main(String[] args) {
-		//creating objects using constructors
-		PersonalDetail perOne = new PersonalDetail("Madhan Kumar S");
-		PersonalDetail perTwo = new PersonalDetail("Joe");
-		
-		//calling methods using objects created
-		perOne.setAge(20);
-		perTwo.setAge(22);
-		
-		perOne.setMobile(9000000000L);
-		perTwo.setMobile(8000000000L);
-		
-		perOne.setLocation("Karur");
-		perTwo.setLocation("Chennai");
-		
-		perOne.printDetail();
-		perTwo.printDetail();
-		
-	}
+        //calling methods using objects created
+        perOne.setAge(20);
+        perTwo.setAge(22);
 
+        perOne.setMobile(9000000000L);
+        perTwo.setMobile(8000000000L);
+
+        perOne.setLocation("Karur");
+        perTwo.setLocation("Chennai");
+
+        perOne.printDetail();
+        perTwo.printDetail();
+    }
 }
