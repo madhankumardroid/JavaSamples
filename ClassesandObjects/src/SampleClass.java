129241388509c7cd94d4d8b7be71e598b6805fef
Madhan Kumar S <35567949+madhankumardroid@users.noreply.github.com>
2018-07-14 21:50:41 +0530

    Alignment
    Made alignment

--- ClassesandObjects/src/SampleClass.java
+++ ClassesandObjects/src/SampleClass.java
@@ -5,7 +5,6 @@ class Sample {
 	Sample(){}
 	Sample(int x){	
 		a=x;
-		
 		System.out.println("The value passed is :"+x);
 	}
 	
@@ -15,25 +14,15 @@ class Sample {
 	
 	public void setValue(int y) {
 		System.out.println("The value to set is :"+y);
-		
-		
 	}
 }
 public class SampleClass {
-
 	public static void main(String[] args) {
-
 		int d;
-		
 		Sample s=new Sample(22); //creation of an object for class Sample
-		
 		System.out.println("The value of variable accessed is :"+s.a); //accessing a variable of class Sample using its object
-		
 		s.setValue(10); //accessing a method of class Sample
-
 		d=s.getValue();
-		
 		System.out.println("The value received is :"+d);
 	}
-
 }
